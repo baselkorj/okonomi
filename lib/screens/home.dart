806cd3cedcc4c32e3d941cf2933c362d82b06402
baselkorj <baselanwarkorj@gmail.com>
2021-07-03 19:55:27 -0400

    added gui for accounts panel

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -48,34 +48,85 @@ class _HomeState extends State<Home> {
       ),
 
       drawer: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
+        // column holds all the widgets in the drawer
+        child: Column(
           children: <Widget>[
             DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
+                padding: EdgeInsets.all(0),
+                child: Container(
+                  color: color1,
+                )),
+            Expanded(
+              // ListView contains a group of widgets that scroll inside the drawer
+              child: ListView(
+                physics: BouncingScrollPhysics(),
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  ExpansionTile(
+                    title: Text(
+                      'Personal',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    leading: CircleAvatar(backgroundColor: color1),
+                    trailing: ElevatedButton(
+                        child: Text('Select',
+                            style: TextStyle(color: Colors.white)),
+                        style: ElevatedButton.styleFrom(primary: color1),
+                        onPressed: () => Navigator.pop(context)),
+                    childrenPadding: EdgeInsets.symmetric(
+                        horizontal: height * 0.02, vertical: height * 0.01),
+                    initiallyExpanded: true,
+                    children: [
+                      Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [Text('Opening Balance'), Text('OMR800')],
+                          ),
+                          SizedBox(height: height * 0.005),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [Text('Income'), Text('OMR320')],
+                          ),
+                          SizedBox(height: height * 0.005),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [Text('Expenses'), Text('OMR20')],
+                          ),
+                          SizedBox(height: height * 0.002),
+                          Divider(
+                              thickness: 2,
+                              indent: 1,
+                              endIndent: 1,
+                              color: color1),
+                          SizedBox(height: height * 0.002),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [Text('Total'), Text('OMR900')],
+                          ),
+                        ],
+                      )
+                    ],
+                  )
+                ],
               ),
-              child: Text('Drawer Header'),
             ),
-            ListTile(
-              title: Text('Item 1'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text('Item 2'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
+            // This container holds the align
+            Container(
+                // This align moves the children to the bottom
+                child: Align(
+                    alignment: FractionalOffset.bottomCenter,
+                    // This container holds all the children that will be aligned
+                    // on the bottom and should not scroll with the above ListView
+                    child: Container(
+                        child: Column(
+                      children: <Widget>[
+                        Divider(),
+                        ListTile(
+                            leading: Icon(Icons.settings, size: height * 0.032),
+                            title: Text('Manage Accounts')),
+                      ],
+                    ))))
           ],
         ),
       ),
