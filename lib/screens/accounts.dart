4e7f79d172d2029d7594d714c45a2feb92330495
baselkorj <baselanwarkorj@gmail.com>
2021-07-03 19:55:53 -0400

    created accounts file

---
+++ lib/screens/accounts.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Accounts extends StatefulWidget {
+  const Accounts({Key? key}) : super(key: key);
+
+  @override
+  _AccountsState createState() => _AccountsState();
+}
+
+class _AccountsState extends State<Accounts> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
