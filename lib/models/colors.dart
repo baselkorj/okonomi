bcbca6bf918ee2dac58f84ae87a564d4f9267356
baselkorj <baselanwarkorj@gmail.com>
2021-07-11 12:11:04 -0400

    added more colors

--- lib/models/colors.dart
+++ lib/models/colors.dart
@@ -5,6 +5,7 @@ const color1 = const Color(0xFFCB576C);
 const color2 = const Color(0xFF4781BE);
 const color3 = const Color(0xFFEDA924);
 const color4 = const Color(0xFF8BBC25);
+const color5 = const Color(0xFFFFA057);
 
 List<Color> gradient1 = [Color(0xFF65C7F7), Color(0xFF3a7bd5)];
 List<Color> gradient2 = [Color(0xFFFBB040), Color(0xFFEF4136)];
