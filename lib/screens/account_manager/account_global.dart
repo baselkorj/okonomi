85acb948b01fdf63a9bc2f54cfff33d73af9d82c
baselkorj <baselanwarkorj@gmail.com>
2021-09-25 03:44:39 +0300

    added color focus to text fields and assigned add

--- lib/screens/account_manager/account_global.dart
+++ lib/screens/account_manager/account_global.dart
@@ -2,5 +2,6 @@ import 'package:flutter/cupertino.dart';
 
 String currentName = '';
 String currentOpenAmount = '0.0';
+String currentGoalLimit = '0.0';
 int currentColor = 0xFFCB576C;
 final currentCurrency = ValueNotifier<String>('AFN');
