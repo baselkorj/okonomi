a9c04292d4dc3d39ddc65f660bad0d319cee0893
baselkorj <baselanwarkorj@gmail.com>
2022-02-06 00:12:54 +0400

    first commit of new date dialog ui

--- lib/screens/home/widgets/dateDialog.dart
+++ lib/screens/home/widgets/dateDialog.dart
@@ -1,44 +1,158 @@
 import 'package:flutter/material.dart';
 import 'package:okonomi/models/lists.dart';
+import 'package:okonomi/models/global.dart';
 
-AlertDialog dateDialog() {
-  int year = DateTime.now().year.toInt();
-  List<Widget> yearTiles = [];
-  List<Widget> monthTiles = [];
+class DateDialog extends StatefulWidget {
+  @override
+  _DateDialogState createState() => _DateDialogState();
+}
 
-  for (var i = year; i > year - 20; i--) {
-    yearTiles.add(ListTile(title: Text('$i')));
-  }
+class _DateDialogState extends State<DateDialog> {
+  int _month = DateTime.now().month;
+  int _year = DateTime.now().year;
 
-  for (var i = 0; i < months.length; i++) {
-    monthTiles.add(ListTile(title: Text('${months[i]}')));
-  }
-
-  return AlertDialog(
-    title: Text(
-      'Select Date',
-      style: TextStyle(height: 1.5),
-    ),
-    content: Container(
-      height: 200,
-      child: Row(
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
-          Flexible(
-            child: ListWheelScrollView(
-              itemExtent: 24,
-              diameterRatio: 0.5,
-              children: monthTiles,
-            ),
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            if (_month != 12) {
+                              _month++;
+                            } else {
+                              _month = 1;
+                            }
+                          });
+                        },
+                        child: Icon(Icons.arrow_drop_up),
+                        style: ElevatedButton.styleFrom(
+                            primary: Color(currentAccount.value.color)),
+                      ),
+                      SizedBox(height: 10.0),
+                      Container(
+                        height: 85,
+                        decoration: BoxDecoration(
+                          color:
+                              Color(currentAccount.value.color).withAlpha(60),
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        ),
+                        child: Center(
+                          child: Text(
+                            '${months[_month]}',
+                            style: TextStyle(
+                                fontSize: 48,
+                                fontWeight: FontWeight.w600,
+                                color: Color(currentAccount.value.color)),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10.0),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            if (_month != 1) {
+                              _month--;
+                            } else {
+                              _month = 12;
+                            }
+                          });
+                        },
+                        child: Icon(Icons.arrow_drop_down),
+                        style: ElevatedButton.styleFrom(
+                            primary: Color(currentAccount.value.color)),
+                      ),
+                    ]),
+              ),
+              SizedBox(
+                width: 20.0,
+              ),
+              Expanded(
+                child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            _year++;
+                          });
+                        },
+                        child: Icon(Icons.arrow_drop_up),
+                        style: ElevatedButton.styleFrom(
+                            primary: Color(currentAccount.value.color)),
+                      ),
+                      SizedBox(height: 10.0),
+                      Container(
+                        height: 85,
+                        decoration: BoxDecoration(
+                          color:
+                              Color(currentAccount.value.color).withAlpha(60),
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        ),
+                        child: Center(
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 12.0),
+                            child: Text(
+                              '$_year',
+                              style: TextStyle(
+                                  fontSize: 48,
+                                  fontWeight: FontWeight.w600,
+                                  color: Color(currentAccount.value.color)),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10.0),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            _year--;
+                          });
+                        },
+                        child: Icon(Icons.arrow_drop_down),
+                        style: ElevatedButton.styleFrom(
+                            primary: Color(currentAccount.value.color)),
+                      ),
+                    ]),
+              )
+            ],
           ),
-          Flexible(
-            child: ListWheelScrollView(
-              itemExtent: 250,
-              diameterRatio: 2,
-              children: yearTiles,
-            ),
+          SizedBox(
+            height: 30.0,
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    'Cancel',
+                    style: TextStyle(color: Color(currentAccount.value.color)),
+                  )),
+              SizedBox(width: 10.0),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Ok'),
+                style: ElevatedButton.styleFrom(
+                    primary: Color(currentAccount.value.color)),
+              )
+            ],
+          )
         ],
       ),
-    ),
-  );
+    );
+  }
 }
