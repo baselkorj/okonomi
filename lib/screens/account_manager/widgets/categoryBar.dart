8366688748137e06778de22d487b42b25571cf76
baselkorj <baselanwarkorj@gmail.com>
2022-02-13 23:53:06 +0400

    initial category bar concept

---
+++ lib/screens/account_manager/widgets/categoryBar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:okonomi/models/style.dart';
+
+Container categoryBar() {
+  List categories = [1, 2, 3, 4];
+  List<Widget> barContents = [];
+  List colors = [color1, color2, color3, color4, color5, color6, color7];
+
+  for (var i = 0; i < categories.length; i++) {
+    barContents.add(Expanded(
+      flex: categories[i],
+      child: Container(
+        color: Color(colors[i]),
+      ),
+    ));
+  }
+
+  return Container(
+    height: 25,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(5.0),
+    ),
+    child: Row(
+      mainAxisSize: MainAxisSize.max,
+      children: barContents,
+    ),
+  );
+}
