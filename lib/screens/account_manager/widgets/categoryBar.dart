c1dcfcbccd59fcec372d3db4d31aee678242fc71
baselkorj <baselanwarkorj@gmail.com>
2022-02-14 20:29:29 +0400

    edit to finals

--- lib/screens/account_manager/widgets/categoryBar.dart
+++ lib/screens/account_manager/widgets/categoryBar.dart
@@ -1,31 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:okonomi/models/style.dart';
 
-Container categoryBar() {
-  List categories = [3, 1, 4, 4, 6, 2, 1];
-  List<Widget> barContents = [];
-  List colors = [color1, color2, color3, color4, color5, color6, color7];
+class CategoryBar extends StatelessWidget {
+  final List categoryAmount;
+  const CategoryBar({Key? key, required this.categoryAmount}) : super(key: key);
 
-  for (var i = 0; i < categories.length; i++) {
-    barContents.add(Expanded(
-      flex: categories[i],
-      child: Container(
-        color: Color(colors[i]),
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> barContents = [];
+    List colors = [color1, color2, color3, color4, color5, color6, color7];
+
+    for (var i = 0; i < categoryAmount.length; i++) {
+      barContents.add(Expanded(
+        flex: categoryAmount[i],
+        child: Container(
+          color: Color(colors[i]),
+        ),
+      ));
+    }
+
+    return Container(
+      height: 25,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5.0),
       ),
-    ));
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(5.0),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: barContents,
+        ),
+      ),
+    );
   }
-
-  return Container(
-    height: 25,
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(5.0),
-    ),
-    child: ClipRRect(
-      borderRadius: BorderRadius.circular(5.0),
-      child: Row(
-        mainAxisSize: MainAxisSize.max,
-        children: barContents,
-      ),
-    ),
-  );
 }
